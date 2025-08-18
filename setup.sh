16585f19142b208e6ffdf1409f5bfbfbc7db7e39
Jack <ttenneb.kcaj@gmail.com>
2025-08-18 20:35:35 +0100

    restructure and rename desktop playbooks

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 GUI_USER="jack"
 REPO_URL="https://github.com/gittyup444/ansible-desktops.git"
-PLAYBOOK="local.yml"
+PLAYBOOK="desktop-apps.yml"
 
 sudo apt install ansible git syncthing
 sudo /usr/bin/ansible-pull -U "$REPO_URL" -e "gui_user=$GUI_USER" "$PLAYBOOK"
